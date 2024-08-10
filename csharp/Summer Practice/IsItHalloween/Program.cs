b754e9f433838390fd025ffd511602e5575cc975
EirikaDoesntCode <eirikadoesntcode@gmail.com>
2024-08-10 20:03:21 +0100

    First solution in new summer practice folder

---
+++ csharp/Summer Practice/IsItHalloween/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IIH
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if(input == "OCT 31")
+            {
+                Console.WriteLine("yup");
+            }
+            else
+            {
+                Console.WriteLine("nope");
+            }
+        }
+    }
+}
